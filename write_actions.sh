d4d78d78ea9e51538ac507eea1e37ca28caf9cc5
LoisaKitakaya <kitakayaloisa@gmail.com>
2023-02-16 10:08:59 -0800

    Some minor changes

--- write_actions.sh
+++ write_actions.sh
@@ -3,4 +3,4 @@ clear
 
 read -p "Enter name of file: " filename
 
-vim 0x02-shell_redirections/$filename && chmod +x 0x02-shell_redirections/$filename
+vim 0x00-hello_world/$filename && chmod +x 0x00-hello_world/$filename
