3b825ae160f1bf58425d5be00a675fd5249c2f72
LoisaKitakaya <kitakayaloisa@gmail.com>
2023-02-16 10:57:11 -0800

    Corrections

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * Return: Always 1 (Success)
  */
 
-int main(void) {
-	fprintf(stderr, "And that piece of art is useful - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
